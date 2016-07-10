fd8c33ddddc4dea573d95184dff9f4b89ac2ebe0
andres <andresdavid@outlook.com>
2016-07-09 23:29:06 -0400

    Merge pull request #7 from alex94puchades/master
    Fix app JSPM client baseUrl

--- src/app/components/+home/home.component.ts
+++ src/app/components/+home/home.component.ts
@@ -7,10 +7,23 @@ import {Component} from "@angular/core";
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-    public pathImage:string;
-    public hello:string;
+    // Variables de Entrada:
+    public costoEspejo  :number;    // Costo del espejo
+    public numEspejo    :number;    // Número de Espejos
+    public invInicial   :number;    // Inversión Inicial
+    public cantAnunc    :number;    // Cantidad de anunciantes
+    public porcSociedad :number;    // Porcentaje de la Sociedad
+    public porcImpues   :number;    // Impuestos
+    public costoDiario  :number;    // Costo  diario
+    public cantEspePub  :number;    // Cantidad de Espejos donde se mostrara la publicidad
+    public tiempContrato:number;    // Tiempo de contrato (meses)
+
+    // Variables de Salida
+    public factDiaria:number;
+    public factMensual:number;
+    public factAnual:number;
+    public mesEstiReIn:number;
     constructor(){
-        this.pathImage="../../../../assets/home/picture.png";
-        this.hello ="hola mundo";
+        
     }
 }
---
+++ src/app/shared/services/src/datos.service.ts
@@ -0,0 +1,9 @@
+/**
+ * Service DatosService
+ */
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class DatosService {
+    
+}
