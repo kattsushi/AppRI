8ddfa3a8468e3e89a4bc69f60174de30182ecc8a
andres <andresdavid@outlook.com>
2016-07-13 17:14:46 -0400

    Implementando Electron

--- src/app/components/+home/home.component.ts
+++ src/app/components/+home/home.component.ts
@@ -2,6 +2,17 @@ import {Component} from "@angular/core";
 import { NgForm }    from "@angular/common";
 // import { DatosService } from '../../shared/service/src/datos.service';
 
+export class Proyec {
+    mes             : number;
+    invIni          : number;
+    FacDiaria       : number;
+    FacMensual      : number;
+    FacAnual        : number;
+    UtiliImp        : number;
+    UtilidadPer     : number;
+}
+
+
 @Component({
     selector: 'home',
     moduleId: module.id,
@@ -10,39 +21,33 @@ import { NgForm }    from "@angular/common";
     directives: [NgForm]
 })
 
-Iproyec interface {
-    
-}
-
 export class HomeComponent {
+    // Variables auxiliares
+    public proyeccion   : Proyec;
     // Variables de Entrada:
-    public costoEspejo  :number;    // Costo del espejo
-    public numEspejo    :number;    // Número de Espejos
-    public invInicial   :number;    // Inversión Inicial
-    public cantAnunc    :number;    // Cantidad de anunciantes
-    public porcSociedad :number;    // Porcentaje de la Sociedad
-    public porcImpues   :number;    // Impuestos
-    public costoDiario  :number;    // Costo  diario
-    public cantEspePub  :number;    // Cantidad de Espejos donde se mostrara la publicidad
-    public tiempContrato:number;    // Tiempo de contrato (meses)
+    public costoEspejo  : number;    // Costo del espejo
+    public numEspejo    : number;    // Número de Espejos
+    public invInicial   : number;    // Inversión Inicial
+    public cantAnunc    : number;    // Cantidad de anunciantes
+    public porcSociedad : number;    // Porcentaje de la Sociedad
+    public porcImpues   : number;    // Impuestos
+    public costoDiario  : number;    // Costo  diario
+    public cantEspePub  : number;    // Cantidad de Espejos donde se mostrara la publicidad
+    public tiempContrato: number;    // Tiempo de contrato (meses)
 
     // Variables de Salida
-    public factDiaria   :number;    // Facturacion diaria
-    public factMensual  :number;    // Facturacion Mensual
-    public factAnual    :number;    // Facturación Anual
-    public factAnualImp :number;    // Facturación Anual luego de impuestos
-    public mesEstiReIn  :number;    // Mes estimado del Retorno de inversión
-    public gananciaMes  :number;    // Ganancia a los 24 meses
+    public factDiaria   : number;    // Facturacion diaria
+    public factMensual  : number;    // Facturacion Mensual
+    public factAnual    : number;    // Facturación Anual
+    public factAnualImp : number;    // Facturación Anual luego de impuestos
+    public mesEstiReIn  : number;    // Mes estimado del Retorno de inversión
+    public gananciaMes  : number;    // Ganancia a los 24 meses
 
     private i :number;
 
         constructor(){
         this.invInic();
         this.invInicial = this.invInic();
-    }
-
-        onProyec(): void {
-
     }
 
     invInic(): number {
@@ -53,7 +58,6 @@ export class HomeComponent {
     }
 
     genProyec(): Array<any> {
-        
         return [];
     }
 }
